c6fb448b52fd296566bfe26cda9111d6aba78f61
Njinju Fogap <andrewfogap@gmail.com>
2023-09-13 20:48:34 +0200

    .

--- ETL-System/Extraction.py
+++ ETL-System/Extraction.py
@@ -1,9 +1,12 @@
 import PyPDF2
 import mlflow
+import os
+ROOT_DIR = os.getcwd()
+DATA_DIR = os.path.join(ROOT_DIR , "AllData")
 
 
-def extraction():
-    with open('../AllData/RawData/Njinju.pdf', 'rb') as pdf_file:
+def extraction(filepath):
+    with open(filepath, 'rb') as pdf_file:
 
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         extracted_text = ""
@@ -13,13 +16,13 @@ def extraction():
             page_text = page.extract_text()
             extracted_text += page_text
 
-    pdf_file.close()
-
     return extracted_text;
 
-extracted_text = extraction()
+if __name__=='__main__':
+    filepath = os.path.join(DATA_DIR, 'RawData','Njinju.pdf')
+    extracted_text = extraction(filepath)
 
-print(extracted_text)
-mlflow.log_text(extracted_text, "extracted_text.txt")
+    print(extracted_text)
+    mlflow.log_text(extracted_text, "extracted_text.txt")
 
-#print("PyPDF2 Version:", mlflow.__version__)
+    #print("PyPDF2 Version:", mlflow.__version__)
