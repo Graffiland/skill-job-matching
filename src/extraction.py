b609126baf8ca72467fdb314b931a41b0cb536f6
Njuaka <93123794+Njuaka@users.noreply.github.com>
2023-09-18 22:28:05 +0200

    llchain

--- src/extraction.py
+++ src/extraction.py
@@ -1,25 +1,78 @@
-import mlflow
 import os
-import utilities
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import create_extraction_chain
 
-DATA_DIR = utilities.getfilepath()
+def getfilepaths():
+    """
+    Function to get file paths for Excel file and API key file.
+    """
+    ROOT_DIR = os.getcwd()
+    ROOT_DIR = os.path.dirname(ROOT_DIR)  # Extract the parent directory from the full path (ETLSystem)
+    DATA_DIR = os.path.join(ROOT_DIR, "AllData", "RawData")
 
-def extraction_cv(filepath):
-    '''
-        This function is use to extract data from the csv or word document
-    '''
-    if filepath.lower().endswith('.docx'):
-        return utilities.wordextractor(filepath) # function use to extract data from a word doc
-    else :    
-        return utilities.pdfextractor(filepath) # function use to extract data from a pdf file
+    # the file paths
+    excelpath = os.path.join(DATA_DIR, "Surveys.xlsx")
+    apipath = os.path.join(DATA_DIR, "api.txt")
+
+    return excelpath, apipath
+
+def extract_surveydata(excelpath, apipath, schema):
+    # Read the API key from a file and set it as an environment variable
+    with open(apipath, 'r') as file:
+        api_key = file.read().strip()
+        os.environ['OPENAI_API_KEY'] = api_key
+
+    data = pd.read_excel(excelpath)
     
+    # Initialize an empty string to store the text
+    text = ""
 
+    # Iterate through the rows of the DataFrame and convert to text
+    for index, row in data.iterrows():
+        # Initialize a sentence for each row
+        sentence = ""
 
+        # Iterate through the columns and add column values to the sentence
+        for column_value in row.iloc[1:]:  # Skip the first column ('id')
+            sentence += str(column_value) + " "
 
-if __name__=='__main__':
-    filepath = os.path.join(DATA_DIR, 'RawData','Njinju.pdf')
-    extracted_text = extraction_cv(filepath)
+        # Add a newline character at the end of the sentence
+        sentence += "\n"
 
-    print(extracted_text)
-    mlflow.log_text(extracted_text, "extracted_text.txt")
-    #print("PyPDF2 Version:", mlflow.__version__)
+        # Append the sentence to the text
+        text += sentence
+
+    # Initialize ChatOpenAI with the specified model and temperature.
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+    # Create an extraction chain based on the provided schema
+    chain = create_extraction_chain(schema, llm)
+
+    # Run the chain on the generated text
+    result = chain.run(text)
+
+    # Return the extracted information
+    return result
+
+# Get file paths using the getfilepaths function
+excelpath, apipath = getfilepaths()
+
+# Define your schema based on the data
+data = pd.read_excel(excelpath)  # Define data here
+
+# Define your schema
+schema = {
+    "properties": {
+        column: {
+            "type": "string" if data[column].dtype == 'object' else "integer"
+        } for column in data.columns
+    },
+    "required": [],  # All columns are required
+}
+
+# Call the function to extract data and run the chain
+result = extract_surveydata(excelpath, apipath, schema)
+
+# Print the extracted result
+print(result)
