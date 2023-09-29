e299afeab66db87fb6792a00316b2bba43652a56
Njinju Fogap <andrewfogap@gmail.com>
2023-09-29 11:20:32 +0200

    >

--- src/transform.py
+++ src/transform.py
@@ -1,11 +1,13 @@
 from extraction import extracted_text
 import re
 
+
 def mask_data(text):
     # Define a list of sensitive information patterns to match
     patterns = [
-                                 # Matches names with the assumption of an uppercase letter followed by lowercase letters
-        r'\+32\s\d+\s\d+\s\d+',  # Matches Belgian phone numbers with the assumption of "+32 <digits> <digits> <digits>"
+        # Matches names with the assumption of an uppercase letter followed by lowercase letters
+        # Matches Belgian phone numbers with the assumption of "+32 <digits> <digits> <digits>"
+        r'\+32\s\d+\s\d+\s\d+',
         r'\S+@\S+'  # Matches email addresses
     ]
 
@@ -20,5 +22,6 @@ def mask_data(text):
     return text
 
 
+data = mask_data(extracted_text)
 
-
+print(data)
