8d8d99ffcb553b13b2ae107cf52b33bb4e8388aa
Njuaka <93123794+Njuaka@users.noreply.github.com>
2023-10-14 08:05:23 +0200

    modified code

--- src/transform.py
+++ src/transform.py
@@ -1,12 +1,31 @@
 from extraction import extracted_survey
-#print(extracted_survey)
-
 import re
 import json
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 def mask_sensitive_data(extracted_survey):
     # Regular expression pattern to match email addresses
     email_pattern = r'\b[\w.-]+@[a-zA-Z.-]+\b'
+
+    # Regular expression pattern to match gender-related words
+    gender_pattern = r'\b(?:male|female|man|woman|boy|girl)\b'
+
+    # Regular expression pattern to match ethnic region-related words
+    ethnic_region_pattern = r'\b(?:race|ethnicity|origin|ethnic background)\b'
+
+    # Religious pattern
+    religious_pattern = r'\b(?:religion|religious beliefs|faith)\b'
+
+    # Genetic pattern
+    genetic_pattern = r'\b(?:genetic information|genetic data)\b'
+
+    # Biometric pattern
+    biometric_pattern = r'\b(?:biometric data|fingerprint|retina scan|iris scan|facial recognition)\b'
+
+    # Health-related pattern
+    health_pattern = r'\b(?:health information|medical data|medical history|disease|condition|treatment|medication)\b'
 
     # to mask email addresses
     def mask_email(match):
@@ -34,7 +53,7 @@ def mask_sensitive_data(extracted_survey):
         masked_item = {}
         for key, value in item.items():
             if key == "Email Address":
-                masked_item[key] = re.sub(email_pattern, mask_email, value)
+                value = re.sub(email_pattern, mask_email, value)
             elif key in ["First/Given names", "Last/Family names"]:
                 if value:
                     masked_name = "***"
@@ -42,17 +61,26 @@ def mask_sensitive_data(extracted_survey):
                 else:
                     masked_item[key] = None
             else:
-                # Mask names and email addresses within text fields
+                # Mask names using regular expressions and country/continent names using spaCy
                 if isinstance(value, str):
                     for name in names_to_mask:
                         value = value.replace(name, '***')
-                    masked_item[key] = re.sub(email_pattern, mask_email, value)
+                    doc = nlp(value)
+                    for ent in doc.ents:
+                        if ent.label_ in ("GPE", "LOC"):  # Filter country and continent names
+                            value = value.replace(ent.text, '***')
+                    value = re.sub(gender_pattern, '***', value, flags=re.IGNORECASE)
+                    value = re.sub(ethnic_region_pattern, '***', value, flags=re.IGNORECASE)
+                    value = re.sub(religious_pattern, '***', value, flags=re.IGNORECASE)
+                    value = re.sub(genetic_pattern, '***', value, flags=re.IGNORECASE)
+                    value = re.sub(biometric_pattern, '***', value, flags=re.IGNORECASE)
+                    value = re.sub(health_pattern, '***', value, flags=re.IGNORECASE)
+                    masked_item[key] = value
                 else:
                     masked_item[key] = value
         masked_data.append(masked_item)
 
     return masked_data
-
 
 # Process the JSON data
 masked_json = mask_sensitive_data(extracted_survey)
